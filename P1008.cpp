83bbf77fe7d395e3b9a57800db1cb54aba6e6f0e
方雨 <44537598+wfybsz@users.noreply.github.com>
2019-07-30 15:11:02 +0800

    Create P1008.cpp

---
+++ P1008.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int check(int t1,int t2,int t3);
+int main()
+{int x;
+for(x=123;x<333;x++)
+if(check(x,2*x,3*x))  
+printf("%d %d %d\n",x,2*x,3*x);
+return 0;
+}
+int check(int t1,int t2,int t3)
+{ int lRc;
+int temp1=45; 
+long temp2=362880;
+long a1=t1%10+t1/10%10+t1/100%10;
+long b1=t2%10+t2/10%10+t2/100%10; 
+long c1=t3%10+t3/10%10+t3/100%10; 
+long a2=(t1%10)*(t1/10%10)*(t1/100%10); 
+long b2=(t2%10)*(t2/10%10)*(t2/100%10);
+long c2=(t3%10)*(t3/10%10)*(t3/100%10);
+if(((a1+b1+c1)==temp1) && (a2*b2*c2==temp2))   
+lRc=1;
+else lRc=0; 
+return lRc;
+}
