55c5bef1354a609d29045f0b44f85d6603e60e9b
Daniel <wfybsz@163.com>
2019-07-30 23:22:24 +0800

    Update AT3529.cpp

--- AT3529.cpp
+++ AT3529.cpp
@@ -1,13 +1,15 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,cnt,tmp1,tmp2;
+
 int main()
 {
-	int n,cnt = 0,tmp1,tmp2;
-	std::cin>>n;
+	cin>>n;
 	for(int i = 1;i <= n;i++){
-		std::cin>>tmp1>>tmp2;
-		cnt += std::abs(tmp1 - tmp2) + 1;
+		cin>>tmp1>>tmp2;
+		cnt += abs(tmp1 - tmp2) + 1;
 	}
-	std::cout<<cnt;
+	cout<<cnt;
 	return 0;
 }
