374c1bb2883b3a5c66db362a55bdc2cebc974f2a
Daniel <wfybsz@163.com>
2019-07-31 12:29:45 +0800

    Create e.g.cpp

---
+++ Unfinished/e.g.cpp
@@ -0,0 +1,16 @@
+//What should put in this folder? For example:
+#include <bits/stdc++.h>
+using namespace std;
+
+const int NR = 1e6;
+int n,a[NR];
+
+int main()
+{
+    cin>>n;
+    for(int i = 0;i < n;i++)
+        cin>>a[i];
+    sort(a,a + n);
+    //The output wasn't finished, so we put it in this folder temporary
+    return 0;
+}
