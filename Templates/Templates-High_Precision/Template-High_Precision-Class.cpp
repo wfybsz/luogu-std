dd49fc2f1e8431b2d53c33c00c4e66d1ac16c8bc
Daniel <wfybsz@163.com>
2019-07-31 20:22:20 +0800

    Update Template-High_Precision-Class.cpp

--- Templates/Templates-High_Precision/Template-High_Precision-Class.cpp
+++ Templates/Templates-High_Precision/Template-High_Precision-Class.cpp
@@ -1,13 +1,130 @@
-class ii{
+class HP{
 private:
-	string a;
+	int len,s[MAXN];
 public:
-	string operator+(const ii b)const{
-		string ans;
-		int na[L] = {0},nb[L] = {0};
-		int la = a.size(),lb = b.a.size();
-		for(int i = 0;i < la;i++)
-			na[la - 1 - i] = a[i] - '0';
-		for(int i = 0;)
+	HP(){
+		memset(s,0,sizeof(s));
+		len = 1;
 	}
+	HP operator=(const char *num){
+		len = strlen(num);
+		for(int i = 0;i < len;i++)
+			s[i] = num[len - i - 1] - '0';
+	}
+	HP operator=(int num){
+		char s[MAXN];
+		sprintf(s,"%d",num);
+		*this = s;
+		return *this;
+	}
+	HP(int num){*this = num;}
+	HP(const char *num){*this = num;}
+	string str()const{
+		string res = "";
+		for(int i = 0;i < len;i++)
+			res = (char)(s[i] +' 0') + res;
+		if(res == "")
+			res = "0";
+		return res;
+	}
+	HP operator+(const HP &b)const{
+		HP c;
+		c.len = 0;
+		for(int i = 0,g = 0;g || i < max(len,b.len);i++)		{
+			int x = g;
+			if(i < len)
+				x += s[i];
+			if(i < b.len)
+				x += b.s[i];
+			c.s[c.len++] = x % 10;
+			g = x / 10;
+		}
+		return c;
+	}
+	void clean(){while(len > 1 && !s[len - 1]) len--;}
+	HP operator*(const HP &b){
+		HP c;
+		c.len = len + b.len;
+		for(int i = 0;i < len;i++)
+		for(int j = 0;j < b.len;j++)
+			c.s[i + j] += s[i] * b.s[j];
+		for(int i = 0;i < c.len - 1;i++){
+		c.s[i + 1] += c.s[i] / 10;
+		c.s[i] %= 10;
+		}
+		c.clean();
+		return c;
+	}
+	HP operator-(const HP &b){
+		HP c;
+		c.len = 0;
+		for(int i = 0,g = 0;i < len;i++){
+			int x = s[i] - g;
+			if(i < b.len)
+				x -= b.s[i];
+			if(x >= 0)
+				g = 0;
+			else{
+				g = 1;
+				x += 10;
+			}
+			c.s[c.len++] = x;
+		}
+		c.clean();
+		return c;
+	}
+	HP operator/(const HP &b){
+		HP c,f = 0;
+		for(int i = len-1; i >= 0; i--){
+			f = f*10;
+			f.s[0] = s[i];
+			while(f >= b){
+				f = f - b;
+				c.s[i]++;
+			}
+		}
+		c.len = len;
+		c.clean();
+		return c;
+	}
+	HP operator%(const HP &b){
+		HP r = *this / b;
+		r = *this - r*b;
+		return r;
+	}
+	HP operator /= (const HP &b){
+		*this  = *this / b;
+		return *this;
+	}
+	HP operator %= (const HP &b){
+		*this = *this % b;
+		return *this;
+	}
+	bool operator<(const HP &b)const{
+		if(len != b.len) return len < b.len;
+		for(int i = len-1; i >= 0; i--)
+		if(s[i] != b.s[i]) return s[i] < b.s[i];
+			return false;
+	}
+	bool operator>(const HP &b)const{return b < *this;}
+	bool operator<=(const HP &b){return !(b < *this);}
+	bool operator==(const HP &b){return !(b < *this) && !(*this < b);}
+	bool operator!=(const HP &b){return !(*this == b);}
+	HP operator+=(const HP &b){
+		*this = *this + b;
+		return *this;
+	}
+	bool operator>=(const HP &b){return *this > b || *this == b;}
 };
+
+istream &operator>>(istream &in, HP& x){
+  string s;
+  in>>s;
+  x = s.c_str();
+  return in;
+}
+
+ostream &operator<<(ostream &out, const HP& x){
+  out<<x.str();
+  return out;
+}
