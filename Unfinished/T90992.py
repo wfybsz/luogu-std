459606508bf6a2102bfc3b4415a85138694a34c6
Daniel <wfybsz@163.com>
2019-08-02 16:14:04 +0800

    add

--- Unfinished/T90992.py
+++ Unfinished/T90992.py
@@ -1,4 +1,21 @@
 import math
-lm = 0
+
+lm = 2
 ms = 0
 ss = 0
+nlm = 0
+nms = 0
+nss = 0
+mod = 19260817
+n = 1000000000000000000
+if n == 0:
+    print(0)
+else:
+    for i in range(1,n):
+        nlm = lm
+        nms = lm + ms
+        nss = ms + ss + ss
+        lm = nlm
+        ms = nms
+        ss = nss
+    print(ss % mod)
