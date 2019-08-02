a31a6ace7f6879d75845388f1733a9134e87f3ff
Daniel <wfybsz@163.com>
2019-08-02 16:28:33 +0800

    del

--- Unfinished/T90993.cpp
+++ Unfinished/T90993.cpp
@@ -1,11 +1,17 @@
-#include <bits/sdtc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-double dist(double x1,double y1,double x2,double y2){
-	return sqrt()
+struct node{
+	int x,y;
+}a,p,s;
+
+double dist(int x1,int y1,int x2,int y2){
+	return sqrt(abs(x1 - x2) * abs(x1 - x2) + abs(y1 - y2) * abs(y1 - y2));
 }
 
 int main()
 {
+	cin>>a.x>>a.y>>p.x>>p.y>>s.x>>s.y;
+	
 	return 0;
 }
