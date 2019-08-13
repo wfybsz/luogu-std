33a6ff3fdd552cd545336b2d20398647c6c67e3d
Daniel <wfybsz@163.com>
2019-08-13 11:01:26 +0800

    Update P1219.cpp

--- Accepted/P1219.cpp
+++ Accepted/P1219.cpp
@@ -6,48 +6,48 @@ int n,a[NR],cnt;
 int flag[NR][NR];
 
 void put(int step,int i){
-    flag[step][i]++;
-    for(int x = step + 1;x <= n;x++){
-        flag[x][step + i - x]++;
-        flag[x][i]++;
-        flag[x][x + i - step]++;
-    }
+	flag[step][i]++;
+	for(int x = step + 1;x <= n;x++){
+		flag[x][step + i - x]++;
+		flag[x][i]++;
+		flag[x][x + i - step]++;
+	}
 }
 
 void undo(int step,int i){
-    flag[step][i]--;
-    for(int x = step + 1;x <= n;x++){
-        flag[x][step + i - x]--;
-        flag[x][i]--;
-        flag[x][x + i - step]--;
-    }
+	flag[step][i]--;
+	for(int x = step + 1;x <= n;x++){
+		flag[x][step + i - x]--;
+		flag[x][i]--;
+		flag[x][x + i - step]--;
+	}
 }
 
 void dfs(int step){
-    if(step > n){
-        if(cnt <= 2){
-            for(int i = 1;i <= n;i++)
-            cout<<a[i]<<' ';
-            puts("");
-        }
-        cnt++;
-        return;
-    }
-    for(int i = 1;i <= n;i++){
-        if(flag[step][i]){
-            continue;
-        }
-        a[step] = i;
-        put(step,i);
-        dfs(step + 1);
-        undo(step,i);
-    }
+	if(step > n){
+		if(cnt <= 2){
+			for(int i = 1;i <= n;i++)
+			cout<<a[i]<<' ';
+			puts("");
+		}
+		cnt++;
+		return;
+	}
+	for(int i = 1;i <= n;i++){
+		if(flag[step][i]){
+			continue;
+		}
+		a[step] = i;
+		put(step,i);
+		dfs(step + 1);
+		undo(step,i);
+	}
 }
 
 int main()
 {
-    cin>>n;
-    dfs(1);
-    cout<<cnt;
-    return 0;
+	cin>>n;
+	dfs(1);
+	cout<<cnt;
+	return 0;
 }
