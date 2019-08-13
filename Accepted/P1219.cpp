0314f2a34558a7464276ee9383fc27ef83547907
Daniel <wfybsz@163.com>
2019-08-13 11:01:09 +0800

    Update P1219.cpp

--- Accepted/P1219.cpp
+++ Accepted/P1219.cpp
@@ -1,43 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int NR = 15;
+const int NR = 100;
 int n,a[NR],cnt;
-bool flag[NR][NR];
+int flag[NR][NR];
+
+void put(int step,int i){
+    flag[step][i]++;
+    for(int x = step + 1;x <= n;x++){
+        flag[x][step + i - x]++;
+        flag[x][i]++;
+        flag[x][x + i - step]++;
+    }
+}
+
+void undo(int step,int i){
+    flag[step][i]--;
+    for(int x = step + 1;x <= n;x++){
+        flag[x][step + i - x]--;
+        flag[x][i]--;
+        flag[x][x + i - step]--;
+    }
+}
 
 void dfs(int step){
-	if(step > n){
-		cnt++;
-		if(cnt > 3)
-			return;
-		for(int i = 1;i <= n;i++)
-			cout<<a[i]<<' ';
-		puts("");
-		return;
-	}
-	bool tmp[NR][NR];
-	memcpy(tmp,flag,sizeof(tmp));
-	for(int i = 1;i <= n;i++){
-		if(flag[step][i])
-			continue;
-		a[step] = i;
-		for(int x = step + 1;x <= n;x++){
-			int y1 = step + i - x,y2 = i,y3 = x + i - step;
-			if(y1 >= 1)
-				flag[x][y1] = true;
-			flag[x][y2] = true;
-			if(y3 <= n)
-				flag[x][y3] = true;
-		}
-		dfs(step + 1);
-		memcpy(flag,tmp,sizeof(tmp));
-	}
+    if(step > n){
+        if(cnt <= 2){
+            for(int i = 1;i <= n;i++)
+            cout<<a[i]<<' ';
+            puts("");
+        }
+        cnt++;
+        return;
+    }
+    for(int i = 1;i <= n;i++){
+        if(flag[step][i]){
+            continue;
+        }
+        a[step] = i;
+        put(step,i);
+        dfs(step + 1);
+        undo(step,i);
+    }
 }
 
 int main()
 {
-	cin>>n;
-	dfs(1);
-	cout<<cnt;
-	return 0;
+    cin>>n;
+    dfs(1);
+    cout<<cnt;
+    return 0;
 }
