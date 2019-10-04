3da45da801b515aae1f4718de3e0d8b9c1c6f6ae
Daniel <wfybsz@163.com>
2019-10-05 01:55:09 +0800

    Create P2782.cpp

---
+++ Accepted/P2782.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int NR = 2e5 + 10;
+int n,dp[NR] = {1};
+struct node{
+	int n,s;
+}a[NR];
+
+bool cmp(node a,node b){
+	return a.n < b.n;
+}
+
+int main()
+{
+	cin>>n;
+	for(int i = 1;i <= n;++i)
+		cin>>a[i].n>>a[i].s;
+	sort(a + 1,a + n + 1,cmp);
+	for(int i = 1;i <= n;++i){
+		//dp[i] = 
+	}
+	return 0;
+}
