463c425453b5a70d9f63f8b5dfda478016c4e022
Daniel <wfybsz@163.com>
2019-10-07 09:46:23 +0800

    Create P3902.cpp

---
+++ Accepted/P3902.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const int NR = 1e5 + 10;
+int n,a[NR];
+
+int main()
+{
+	cin>>n;
+	for(int i = 1;i <= n;++i)
+		cin>>a[i];
+	return 0;
+}
