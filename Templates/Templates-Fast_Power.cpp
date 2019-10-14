8eb3c863b90367e3267e2fcf77d3b89b7c6bc235
Daniel <wfybsz@163.com>
2019-10-14 18:38:02 +0800

    Update Templates-Fast_Power.cpp

--- Templates/Templates-Fast_Power.cpp
+++ Templates/Templates-Fast_Power.cpp
@@ -1,10 +1,10 @@
 long long fp(int x,int y){
-    int ret = 1;
-    while(y){
-        if(y & 1)
-            ret *= x;
-        x *= x;
-        y >>= 1;
-    }
-    return ret;
+	int ret = 1;
+	while(y){
+		if(y & 1)
+			ret *= x;
+		x *= x;
+		y >>= 1;
+	}
+	return ret;
 }
