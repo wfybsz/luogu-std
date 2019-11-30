9b3cee66d74b5e5a12d94b0cec89c5e016feaa32
Daniel <wfybsz@163.com>
2019-11-30 22:29:19 +0800

    Update P2340.cpp

--- Accepted/P2340.cpp
+++ Accepted/P2340.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MR = 3.5e4;
 const int NR = 410;
+const int MR = 3.5e4;
 int n,iq[NR],eq[NR],dp[NR][2 * MR + 10],ans;
 
 int main()
@@ -15,12 +15,12 @@ int main()
 	for(int i = 1;i <= n;++i)
 		for(int j = -MR;j <= MR;++j){
 			dp[i][j + MR] = dp[i - 1][j + MR];
-			if(0 <= j + MR -iq[i] && j + MR - iq[i] <= 2 * MR)
-				dp[i][j + MR] = max(dp[i][j + MR],dp[i - 1][j + MR -iq[i]] + eq[i]);
+			if(0 <= j + MR - iq[i] and j + MR - iq[i] <= 2 * MR)
+				dp[i][j + MR] = max(dp[i][j + MR],dp[i - 1][j + MR - iq[i]] + eq[i]);
 		}
-	for(int j = 0;j <= MR;++j)
-		if(dp[n][j + MR] >= 0)
-			ans = max(ans,dp[n][j + MR] + j);
+	for(int i = 0;i <= MR;++i)
+		if(dp[n][i + MR] >= 0)
+			ans = max(ans,dp[n][i + MR] + j);
 	cout<<ans<<endl;
 	return 0;
 }
