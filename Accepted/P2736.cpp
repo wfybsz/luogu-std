e7fae2f70e4c9290215579071c7cce32d59fd573
Daniel <wfybsz@163.com>
2020-02-03 23:31:00 +0800

    qwq

---
+++ Accepted/P2736.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int NR = 40;
+int n,t,m,dp[NR][NR][NR];
+
+int main()
+{
+    cin>>n>>t>>m;
+    for(int i = 1;i <= n;++i)
+        
+    return 0;
+}
