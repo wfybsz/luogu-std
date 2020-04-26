7f6fa817c5bd42086a988aeaa4ba70533d188429
Daniel <wfybsz@163.com>
2020-04-26 18:36:47 +0800

    Update P2670.cpp

--- Accepted/P2670.cpp
+++ Accepted/P2670.cpp
@@ -1,43 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 const int NR = 1e2 + 10;
 int n,m,a[NR][NR];
+bool flag[NR][NR];
+char s[NR];
 
-void Do(int& a){
-	if(a == -1)
-		return;
-	++a;
+int check(int x,int y){
+	int ret = 0;
+	if(flag[x - 1][y])++ret;
+	if(flag[x][y - 1])++ret;
+	if(flag[x - 1][y - 1])++ret;
+	if(flag[x + 1][y])++ret;
+	if(flag[x][y + 1])++ret;
+	if(flag[x + 1][y + 1])++ret;
+	if(flag[x - 1][y + 1])++ret;
+	if(flag[x + 1][y - 1])++ret;
+	return ret;
 }
 
 int main()
 {
 	cin>>n>>m;
-	for(int i = 1;i <= n;++i)
-		for(int j = 1;j <= m;)
-			switch(getchar()){
-				case '*':a[i][j++] = -1;
-				case '?':++j;
-			}
-	for(int i = 1;i <= n;++i)
-		for(int j = 1;j <= m;++j)
-			if(a[i][j] == -1){
-				Do(a[i - 1][j - 1]);
-				Do(a[i - 1][j]);
-				Do(a[i - 1][j + 1]);
-				Do(a[i][j - 1]);
-				Do(a[i][j + 1]);
-				Do(a[i + 1][j - 1]);
-				Do(a[i + 1][j]);
-				Do(a[i + 1][j + 1]);
-			}
+	for(int i = 1;i <= n;++i){
+		cin>>s;
+		for(int j = 0;j < m;++j)
+			flag[i][j + 1] |= s[j] == '*';
+	}
 	for(int i = 1;i <= n;++i){
 		for(int j = 1;j <= m;++j)
-			if(a[i][j] == -1)
+			if(flag[i][j])
 				putchar('*');
 			else
-				cout<<a[i][j];
-		cout<<endl;
+				cout<<check(i,j);
+		putchar('\n');
 	}
 	return 0;
 }
