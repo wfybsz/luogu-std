5f4e0bcf57d56fde4a6756cc016d398e768c7d22
Daniel <wfybsz@163.com>
2020-07-14 17:54:14 +0800

    Update P1462.cpp

--- Accepted/P1462.cpp
+++ Accepted/P1462.cpp
@@ -7,9 +7,14 @@ namespace io{inline int read(){register int x=0,flag=1;char c=getchar();while(c<
 
 const int NR = 1e4 + 10;
 const int MR = 1e5 + 10;
-int n,m,u,v,w,size,head[NR],dis[NR],cnt[NR],node[NR];
+int n,m,b,u,v,w,size,head[NR],dis[NR],cnt[NR],node[NR],node_[NR],l = 1,r,mid,ans;
 bool vis[NR];
-struct edge{int to,next,w;}e[MR];
+struct edge{int to,next,w;}g[MR];
+
+void add(int u,int v,int w){
+	g[++size] = (edge){v,head[u],w};
+	head[u] = size;
+}
 
 bool spfa(int s,int maxn){
 	queue<int>q;
@@ -21,13 +26,43 @@ bool spfa(int s,int maxn){
 		u = q.front();
 		q.pop();
 		vis[u] = false;
-		for(int i = head[u];i;i = e[i].next){
-			v = e[i].to;
+		for(int i = head[u];i;i = g[i].next){
+			v = g[i].to;
+			if(dis[u] + g[i].w < dis[v] and node[v] <= maxn){
+				dis[v] = dis[u] + g[i].w;
+				if(!vis[v]){
+					q.push(v);
+					vis[v] = true;
+				}
+			}
 		}
 	}
+	return dis[n] <= b;
 }
 
 int main()
 {
+	n = r = in;m = in;b = in;
+	for(int i = 1;i <= n;++i)
+		node[i] = node_[i] = in;
+	for(int i = 1;i <= m;++i){
+		u = in;v = in;w = in;
+		add(u,v,w);
+		add(v,u,w);
+	}
+	sort(node_ + 1,node_ + n + 1);
+	if(!spfa(1,2147483647)){
+		printf("AFK\n");
+		return 0;
+	}
+	while(l <= r){
+		mid = (l + r) / 2;
+		if(spfa(1,node_[mid])){
+			ans = node_[mid];
+			r = mid - 1;
+		}else
+			l = mid + 1;
+	}
+	print(ans);
 	return 0;
 }
