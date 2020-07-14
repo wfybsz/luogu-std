85e9ee8c3b1054067b70519033c54d2a943f0909
Daniel <wfybsz@163.com>
2020-07-14 14:18:58 +0800

    qwq

--- Accepted/P1993.cpp
+++ Accepted/P1993.cpp
@@ -6,7 +6,7 @@ namespace io{inline int read(){register int x=0,flag=1;char c=getchar();while(c<
 #define print io::write
 
 const int NR = 5e3 + 10;
-int n,m,u,v,w,size,head[NR],dis[NR],cnt[NR],minn = 2147483647;
+int n,m,x,u,v,w,size,head[NR],dis[NR],cnt[NR];
 bool vis[NR];
 struct edge{int to,next,w;}g[NR<<1];
 
@@ -17,7 +17,7 @@ void add(int u,int v,int w){
 
 bool spfa(int s){
 	queue<int>q;
-	memset(dis,-1,sizeof(dis));
+	memset(dis,999999,sizeof(dis));
 	dis[s] = 0;
 	q.push(s);
 	while(!q.empty()){
@@ -26,7 +26,7 @@ bool spfa(int s){
 		vis[u] = false;
 		for(int i = head[u];i;i = g[i].next){
 			v = g[i].to;
-			if(dis[u] + g[i].w > dis[v]){
+			if(dis[u] + g[i].w < dis[v]){
 				dis[v] = dis[u] + g[i].w;
 				cnt[v] = cnt[u] + 1;
 				if(!vis[v]){
@@ -44,17 +44,24 @@ bool spfa(int s){
 int main()
 {
 	n = in;m = in;
-	for(int i = 1;i <= m;++i){
-		u = in;v = in;w = in;
-		add(u,v,-w);
-	}
+	for(int i = 1;i <= m;++i)
+		switch(in){
+			case 1:{
+				u = in;v = in;w = in;
+				add(u,v,-w);
+			}break;
+			case 2:{
+				u = in;v = in;w = in;
+				add(v,u,w);
+			}break;
+			case 3:{
+				u = in;v = in;
+				add(u,v,0);
+				add(v,u,0);
+			}break;
+		}
 	for(int i = 1;i <= n;++i)
 		add(n + 1,i,0);
-	if(spfa(n + 1))
-		printf("NO\n");
-	else{
-		for(int i = 1;i <= n;++i)
-			print(dis[i],' ');
-	}
+	printf(spfa(n + 1)?"No\n":"Yes\n");
 	return 0;
 }
