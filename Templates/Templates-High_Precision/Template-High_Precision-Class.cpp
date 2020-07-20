8665e0c5c5ee4ce04ceb7d04a692d856f6cbc0e5
Daniel <wfybsz@163.com>
2020-07-20 13:35:36 +0800

    Update Template-High_Precision-Class.cpp

--- Templates/Templates-High_Precision/Template-High_Precision-Class.cpp
+++ Templates/Templates-High_Precision/Template-High_Precision-Class.cpp
@@ -1,409 +1,355 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+#define __BIGINT_MAXIMUM_LENGTH__ 10000
+#define __BIGINT_BASE__ 100000000
 
-class DividedByZeroException{};
-class Int{
-private:
-	vector<char>digits;
-	bool sign;
-	void trim();
-public:
-	Int(int);
-	Int(string&) ;
-	Int();
-	Int(const Int&);
-	Int operator=(const Int& op2);
-	Int abs() const;
-	Int pow(int a);
-	friend Int operator+=(Int&,const Int&);
-	friend Int operator-=(Int&,const Int&);
-	friend Int operator*=(Int&,const Int&);
-	friend Int operator/=(Int&,const Int&) throw(DividedByZeroException);
-	friend Int operator%=(Int&,const Int&) throw(DividedByZeroException);
-	friend Int operator+(const Int&,const Int&);
-	friend Int operator-(const Int&,const Int&);
-	friend Int operator*(const Int&,const Int&);
-	friend Int operator/(const Int&,const Int&) throw(DividedByZeroException);
-	friend Int operator%(const Int&,const Int&) throw(DividedByZeroException);
-	friend Int operator-(const Int&);
-	friend Int operator++(Int&);
-	friend Int operator++(Int&,int);
-	friend Int operator--(Int&);
-	friend Int operator--(Int&,int);
-	friend bool operator>(const Int&,const Int&);
-	friend bool operator<(const Int&,const Int&);
-	friend bool operator==(const Int&,const Int&);
-	friend bool operator!=(const Int&,const Int&);
-	friend bool operator>=(const Int&,const Int&);
-	friend bool operator<=(const Int&,const Int&);
-	friend ostream& operator<<(ostream&,const Int&);
-	friend istream& operator>>(istream&,Int&);
-public:
-	static const Int ZERO;
-	static const Int ONE;
-	static const Int TEN;
-};
-const Int Int::ZERO = Int(0);
-const Int Int::ONE = Int(1);
-const Int Int::TEN = Int(10);
-
-Int::Int(){
-	sign = true;
-}
-
-Int::Int(int val){
-	if(val >= 0)
-		sign = true;
-	else{
-		sign = false;
-		val *= (-1);
+struct ii {
+	static char s[__BIGINT_MAXIMUM_LENGTH__*10];
+	typedef int _T;
+	_T a[__BIGINT_MAXIMUM_LENGTH__];
+	int len;
+	void clear() {
+		len = 1;
+		a[len] = 0;
 	}
-	do{
-		digits.push_back((char)(val % 10));
-		val /= 10;
-	}while(val != 0);
-}
-
-Int::Int(string& def){
-	sign = true;
-	for(string::reverse_iterator iter = def.rbegin() ;iter < def.rend(); iter++){
-		char ch = (*iter);
-		if(iter == def.rend() - 1){
-			if(ch == '+')
-				break;
-			if(ch == '-'){
-				sign = false;
-				break;
-			}
-		}
-		digits.push_back((char)((*iter) - '0'));
+	ii() {
+		clear();
 	}
-	trim();
-}
-
-void Int::trim(){
-	vector<char>::reverse_iterator iter = digits.rbegin();
-	while(!digits.empty() && (*iter) == 0){
-		digits.pop_back();
-		iter = digits.rbegin();
+	ii(_T x) {
+		*this = x;
 	}
-	if(digits.size() == 0){
-		sign = true;
-		digits.push_back(0);
+	ii(char s[]) {
+		this->str(s);
 	}
-}
-
-Int::Int(const Int& op2){
-	sign = op2.sign;
-	digits = op2.digits;
-}
-
-Int Int::operator=(const Int& op2){
-	digits = op2.digits;
-	sign = op2.sign;
-	return(*this);
-}
-
-Int Int::abs()const{
-	if(sign)
+	ii operator=(const ii &b) {
+		len = b.len;
+		for (int i = 1; i <= len; ++i)
+			a[i] = b.a[i];
 		return *this;
-	else
-		return -(*this);
-}
-
-Int Int::pow(int a){
-	Int res(1);
-	for(int i = 0;i < a;i++)
-		res *= (*this);
-	return res;
-}
-
-Int operator+=(Int& op1,const Int& op2){
-	if(op1.sign == op2.sign){
-		vector<char>::iterator iter1;
-		vector<char>::const_iterator iter2;
-		iter1 = op1.digits.begin();
-		iter2 = op2.digits.begin();
-		char to_add = 0;
-		while(iter1 != op1.digits.end() && iter2 != op2.digits.end()){
-			(*iter1) = (*iter1) +(*iter2) + to_add;
-			to_add = ((*iter1) > 9);
-			(*iter1) = (*iter1) % 10;
-			iter1++;
-			iter2++;
-		}
-		while(iter1 != op1.digits.end()){
-			(*iter1) = (*iter1) + to_add;
-			to_add = ((*iter1) > 9);
-			(*iter1) %= 10;
-			iter1++;
-		}
-		while(iter2 != op2.digits.end()){
-			char val = (*iter2) + to_add;
-			to_add = (val > 9);
-			val %= 10;
-			op1.digits.push_back(val);
-			iter2++;
-		}
-		if(to_add != 0)
-			op1.digits.push_back(to_add);
-		return op1;
 	}
-	else
-		if(op1.sign)
-			return op1 -= (-op2);
-		else
-			return op1 = op2 -(-op1);
-}
-
-Int operator-=(Int& op1,const Int& op2){
-	if(op1.sign == op2.sign){
-		if(op1.sign)
-			if(op1 < op2)
-				return	op1 = -(op2 - op1);
-		else
-			if(-op1 > -op2)
-				return op1 = -((-op1) -(-op2));
+	ii operator+(const ii &b) {
+		int L = len > b.len ? len : b.len;
+		ii tmp;
+		for (int i = 1; i <= L + 1; ++i)
+			tmp.a[i] = 0;
+		for (int i = 1; i <= L; ++i) {
+			if (i > len)
+				tmp.a[i] += b.a[i];
 			else
-				return op1 = (-op2) -(-op1);
-		vector<char>::iterator iter1;
-		vector<char>::const_iterator iter2;
-		iter1 = op1.digits.begin();
-		iter2 = op2.digits.begin();
-		char to_substract = 0;
-		while(iter1 != op1.digits.end() && iter2 != op2.digits.end()){
-			(*iter1) = (*iter1) - (*iter2) - to_substract;
-			to_substract = 0;
-			if((*iter1) < 0){
-				to_substract = 1;
-				(*iter1) += 10;
-			}
-			iter1++;
-			iter2++;
-		}
-		while(iter1 != op1.digits.end()){
-			(*iter1) = (*iter1) - to_substract;
-			to_substract = 0;
-			if((*iter1) < 0){
-				to_substract = 1;
-				(*iter1) += 10;
-			}
-			else
-				break;
-			iter1++;
-		}
-		op1.trim();
-		return op1;
-	}
-	else
-		if(op1 > Int::ZERO)
-			return op1 += (-op2);
-		else
-			return op1 = -(op2 +(-op1));
-}
-
-Int operator*=(Int& op1,const Int& op2){
-	Int result(0);
-	if(op1 == Int::ZERO || op2 == Int::ZERO)
-		result = Int::ZERO;
-	else{
-		vector<char>::const_iterator iter2 = op2.digits.begin();
-		while(iter2 != op2.digits.end()){
-			if(*iter2 != 0){
-				deque<char>temp(op1.digits.begin(),op1.digits.end());
-				char to_add = 0;
-				deque<char>::iterator iter1 = temp.begin();
-				while(iter1 != temp.end()){
-					(*iter1) *= (*iter2);
-					(*iter1) += to_add;
-					to_add = (*iter1) / 10;
-					(*iter1) %= 10;
-					iter1++;
+				if (i > b.len)
+					tmp.a[i] += a[i];
+				else {
+					tmp.a[i] += a[i] + b.a[i];
+					if (tmp.a[i] >= __BIGINT_BASE__) {
+						tmp.a[i] -= __BIGINT_BASE__;
+						++tmp.a[i + 1];
+					}
 				}
-				if(to_add != 0)
-					temp.push_back(to_add);
-				int num_of_zeros = iter2 - op2.digits.begin();
-				while(num_of_zeros--)
-					temp.push_front(0);
-				Int temp2;
-				temp2.digits.insert(temp2.digits.end(),temp.begin(),temp.end());
-				temp2.trim();
-				result = result + temp2;
+		}
+		if (tmp.a[L + 1])
+			tmp.len = L + 1;
+		else
+			tmp.len = L;
+		return tmp;
+	}
+	ii operator-(ii b) {
+		int L = len > b.len ? len : b.len;
+		ii tmp;
+		for (int i = 1; i <= L + 1; ++i)
+			tmp.a[i] = 0;
+		for (int i = 1;i <= L; ++i) {
+			if (i > b.len)
+				b.a[i] = 0;
+			tmp.a[i] += a[i] - b.a[i];
+			if (tmp.a[i] < 0) {
+				tmp.a[i] += __BIGINT_BASE__;
+				--tmp.a[i + 1];
 			}
-			iter2++;
 		}
-		result.sign = ((op1.sign && op2.sign) || (!op1.sign && !op2.sign));
+		while (L > 1 and !tmp.a[L])
+			--L;
+		tmp.len = L;
+		return tmp;
 	}
-	op1 = result;
-	return op1;
-}
-
-Int operator/=(Int& op1,const Int& op2)throw(DividedByZeroException){
-	if(op2 == Int::ZERO)
-		throw DividedByZeroException();
-	Int t1 = op1.abs(),t2 = op2.abs();
-	if(t1 < t2){
-		op1 = Int::ZERO;
-		return op1;
+	ii operator*(const ii &b) const {
+		int L = len + b.len;
+		ii tmp;
+		for (int i = 1; i <= L; ++i)
+			tmp.a[i] = 0;
+		for (int i = 1; i <= len; ++i)
+			for (int j = 1; j <= b.len; ++j) {
+				tmp.a[i + j - 1] += a[i] * b.a[j];
+				if (tmp.a[i + j - 1] >= __BIGINT_BASE__) {
+					tmp.a[i + j] += tmp.a[i + j - 1] / __BIGINT_BASE__;
+					tmp.a[i + j - 1] %= __BIGINT_BASE__;
+				}
+			}
+		tmp.len = len + b.len;
+		while (tmp.len > 1 and !tmp.a[tmp.len])
+			--tmp.len;
+		return tmp;
 	}
-	deque<char> temp;
-	vector<char>::reverse_iterator iter = t1.digits.rbegin();
-	Int temp2(0);
-	while(iter != t1.digits.rend()){
-		temp2 = temp2 * Int::TEN + Int((int)(*iter));
-		char s = 0;
-		while(temp2 >= t2){
-			temp2 = temp2 - t2;
-			s = s + 1;
+	pair<ii, ii> divide(const ii &a,const ii &b) {
+		int L = a.len;
+		ii c, d;
+		for (int i = L; i; --i) {
+			c.a[i] = 0;
+			d = d * __BIGINT_BASE__;
+			d.a[1] = a.a[i];
+			int l = 0, r = __BIGINT_BASE__ - 1, mid;
+			while (l < r) {
+				mid = (l + r + 1) >> 1;
+				if (b * mid <= d)
+					l = mid;
+				else
+					r = mid - 1;
+			}
+			c.a[i] = l;
+			d -= b * l;
 		}
-		temp.push_front(s);
-		iter++;
+		while (L>  1 and !c.a[L])
+			--L;
+		c.len = L;
+		return make_pair(c, d);
 	}
-	op1.digits.clear();
-	op1.digits.insert(op1.digits.end(),temp.begin(),temp.end());
-	op1.trim();
-	op1.sign = ((op1.sign && op2.sign) || (!op1.sign && !op2.sign));
-	return op1;
-}
-
-Int operator%=(Int& op1,const Int& op2)throw(DividedByZeroException){
-	return op1 -= ((op1 / op2) * op2);
-}
-
-Int operator+(const Int& op1,const Int& op2){
-	Int temp(op1);
-	temp += op2;
-	return temp;
-}
-
-Int operator-(const Int& op1,const Int& op2){
-	Int temp(op1);
-	temp -= op2;
-	return temp;
-}
-
-Int operator*(const Int& op1,const Int& op2){
-	Int temp(op1);
-	temp *= op2;
-	return temp;
-}
-
-Int operator/(const Int& op1,const Int& op2)throw(DividedByZeroException){
-	Int temp(op1);
-	temp /= op2;
-	return temp;
-}
-
-Int operator%(const Int& op1,const Int& op2)throw(DividedByZeroException){
-	Int temp(op1);
-	temp %= op2;
-	return temp;
-}
-
-Int operator-(const Int& op){
-	Int temp = Int(op);
-	temp.sign = !temp.sign;
-	return temp;
-}
-
-Int operator++(Int& op){
-	op += Int::ONE;
-	return op;
-}
-
-Int operator++(Int& op,int x){
-	Int temp(op);
-	++op;
-	return temp;
-}
-
-Int operator--(Int& op){
-	op -= Int::ONE;
-	return op;
-}
-
-Int operator--(Int& op,int x){
-	Int temp(op);
-	--op;
-	return temp;
-}
-
-bool operator<(const Int& op1,const Int& op2){
-	if(op1.sign != op2.sign)
-		return !op1.sign;
-	else{
-		if(op1.digits.size() != op2.digits.size())
-			return(op1.sign && op1.digits.size() < op2.digits.size())
-				||(!op1.sign && op1.digits.size() > op2.digits.size());
-		vector<char>::const_reverse_iterator iter1,iter2;
-		iter1 = op1.digits.rbegin();
-		iter2 = op2.digits.rbegin();
-		while(iter1 != op1.digits.rend()){
-			if(op1.sign &&	*iter1 < *iter2)
-				return true;
-			if(op1.sign &&	*iter1 > *iter2)
-				return false;
-			if(!op1.sign &&	*iter1 > *iter2)
-				return true;
-			if(!op1.sign &&	*iter1 < *iter2)
-				return false;
-			iter1++;
-			iter2++;
+	ii operator/(_T x) {
+		_T d = 0;
+		ii tmp;
+		for (int i = len; i; --i) {
+			d = d * __BIGINT_BASE__ + a[i];
+			tmp.a[i] = d / x;
+			d %= x;
 		}
-		return false;
+		tmp.len = len;
+		while (tmp.len > 1 and !tmp.a[tmp.len])
+			--tmp.len;
+		return tmp;
 	}
-}
-
-bool operator==(const Int& op1,const Int& op2){
-	if(op1.sign != op2.sign	|| op1.digits.size() != op2.digits.size())
-		return false;
-	vector<char>::const_iterator iter1,iter2;
-	iter1 = op1.digits.begin();
-	iter2 = op2.digits.begin();
-	while(iter1 != op1.digits.end()){
-		if(*iter1 != *iter2) 
-			return false;
-		iter1++;
-		iter2++;
+	_T operator%(_T x){
+		_T d = 0;
+		for (int i = len; i; --i)
+			d = (d * __BIGINT_BASE__ + a[i]) % x;
+		return d;
 	}
-	return true;
+	ii operator/(const ii &b) {
+		return divide(*this, b).first;
+	}
+	ii operator%(const ii &b) {
+		return divide(*this, b).second;
+	}
+	ii &operator+=(const ii &b) {
+		*this = *this + b;
+		return *this;
+	}
+	ii &operator-=(const ii &b) {
+		*this = *this - b;
+		return *this;
+	}
+	ii &operator*=(const ii &b) {
+		*this = *this * b;
+		return *this;
+	}
+	ii operator/=(const ii &b) {
+		*this = *this / b;
+		return *this;
+	}
+	ii operator%=(const ii &b) {
+		*this = *this % b;
+		return *this;
+	}
+	ii &operator++() {
+		ii T;
+		T = 1;
+		*this = *this + T;
+		return *this;
+	}
+	ii &operator--(){
+		ii T;
+		T = 1;
+		*this = *this - T;
+		return *this;
+	}
+	ii operator++(int) {
+		ii T, tmp = *this;
+		T = 1;
+		*this = *this + T;
+		return tmp;
+	}
+	ii operator--(int) {
+		ii T, tmp = *this;
+		T = 1;
+		*this = *this - T;
+		return tmp;
+	}
+	ii operator+(_T x) {
+		ii T;
+		T = x;
+		return *this + T;
+	}
+	ii operator-(_T x) {
+		ii T;
+		T = x;
+		return *this - T;
+	}
+	ii operator*(_T x) {
+		ii T;
+		T = x;
+		return *this * T;
+	}
+	ii operator*=(_T x) {
+		*this = *this * x;
+		return *this;
+	}
+	ii operator+=(_T x) {
+		*this = *this + x;
+		return *this;
+	}
+	ii operator-=(_T x) {
+		*this = *this - x;
+		return *this;
+	}
+	ii operator/=(_T x) {
+		*this = *this / x;
+		return *this;
+	}
+	ii operator%=(_T x) {
+		*this = *this % x;
+		return *this;
+	}
+	bool operator==(_T x) {
+		ii T;
+		T = x;
+		return *this == T;
+	}
+	bool operator!=(_T x) {
+		ii T;
+		T =x;
+		return *this != T;
+	}
+	bool operator<=(_T x) {
+		ii T;
+		T = x;
+		return *this <= T;
+	}
+	bool operator>=(_T x) {
+		ii T;
+		T = x;
+		return *this >= T;
+	}
+	bool operator<(_T x) {
+		ii T;
+		T = x;
+		return *this < T;
+	}
+	bool operator>(_T x) {
+		ii T;
+		T = x;
+		return *this > T;
+	}
+	ii operator=(_T x) {
+		len = 0;
+		while (x)
+			a[++len] = x % __BIGINT_BASE__, x /= __BIGINT_BASE__;
+		if (!len)
+			a[++len] = 0;
+		return *this;
+	}
+	bool operator<(const ii &b) {
+		if (len < b.len)
+			return 1;
+		if (len > b.len)
+			return 0;
+		for (int i = len; i; --i) {
+			if (a[i] < b.a[i])
+				return 1;
+			if (a[i] > b.a[i])
+				return 0;
+		}
+		return 0;
+	}
+	bool operator==(const ii &b) {
+		if (len != b.len)
+			return 0;
+		for (int i = len; i; --i)
+			if (a[i] != b.a[i])
+				return 0;
+		return 1;
+	}
+	bool operator!=(const ii &b) {
+		return !(*this == b);
+	}
+	bool operator>(const ii &b) {
+		return !(*this < b || *this == b);
+	}
+	bool operator<=(const ii &b) {
+		return (*this < b) || (*this == b);
+	}
+	bool operator>=(const ii &b) {
+		return (*this > b) || (*this == b);
+	}
+	void str(char s[]) {
+		int l = strlen(s);
+		_T x = 0, y = 1;
+		len = 0;
+		for (int i = l - 1; i >= 0; --i) {
+			x = x + (s[i] - '0') * y;
+			y *= 10;
+			if (y == __BIGINT_BASE__)
+				a[++len] = x, x = 0, y = 1;
+		}
+		if (!len || x)
+			a[++len] = x;
+	}
+	void read(){
+		scanf("%s", s);
+		this->str(s);
+	}
+	void print(){
+		printf("%d", (int)a[len]);
+		for (int i = len - 1; i; --i) {
+			for (int j = __BIGINT_BASE__ / 10; j >= 10; j /= 10) {
+				if (a[i] < j)
+					putchar('0');
+				else
+					break;
+			}
+			printf("%d", (int)a[i]);
+		}
+	}
+};
+ 
+ii gcd(ii a, ii b) {
+	ii c;
+	c.clear();
+	c = 1;
+	while ((a % 2 == 0) and (b % 2 == 0)) {
+		a /= 2;
+		b /= 2;
+		c *= 2;
+	}
+	while (a % 2 == 0)
+		a /= 2;
+	while (b % 2 == 0)
+		b/=2;
+	while (a != b) {
+		if (a > b)
+			a -= b;
+		else
+			b -= a;
+	}
+	c *= a;
+	return c;
 }
 
-bool operator!=(const Int& op1,const Int& op2){
-	return !(op1 == op2);
+istream &operator>>(istream &is, ii &x) {
+	x.read();
+	return is;
 }
 
-bool operator>=(const Int& op1,const Int& op2){
-	return(op1 > op2) ||(op1 == op2);
-}
-
-bool operator<=(const Int& op1,const Int& op2){
-	return(op1 < op2) ||(op1 == op2);
-}
-
-bool operator>(const Int& op1,const Int& op2){
-	return !(op1 <= op2);
-}
-
-ostream& operator<<(ostream& stream,const Int& val){
-	if(!val.sign)
-		stream<<"-";
-	for(vector<char>::const_reverse_iterator iter = val.digits.rbegin();iter != val.digits.rend() ;iter++)
-		stream<<(char)((*iter) + '0');
-	return stream;
-}
-
-istream& operator>>(istream& stream,Int& val){
-	string str;
-	stream>>str;
-	val = Int(str);
-	return stream;
-}
-
-int main()
-{
-	Int a,b;
-	cin>>a>>b;
-	cout<<a + b;
-	return 0;
+ostream &operator<<(ostream &os, ii &x) {
+	x.print();
+	return os;
 }
